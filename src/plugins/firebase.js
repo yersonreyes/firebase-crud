3cacdc2eb4190e26de575078e4f0bbc7911f7cff
Yerson Reyes <yerson.programador@gmail.com>
2022-07-02 00:41:24 -0400

    proyecto terminado

---
+++ src/plugins/firebase.js
@@ -0,0 +1,10 @@
+import Firebase from "firebase";
+
+Firebase.initializeApp({
+  apiKey: "AIzaSyCgTNjt_ezHXFEaFA5KeAKYZLhWrJ-uMOA",
+  authDomain: "vue-firebase-8f54d.firebaseapp.com",
+  projectId: "vue-firebase-8f54d",
+  storageBucket: "vue-firebase-8f54d.appspot.com",
+  messagingSenderId: "101835878650",
+  appId: "1:101835878650:web:71fb56f77cb0f9b9ff9943",
+});
